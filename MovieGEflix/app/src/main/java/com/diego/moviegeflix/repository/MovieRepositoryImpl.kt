a3efb98a523aa81126afa18500216adb738be4f7
Diego <estradadg7@gmail.com>
2021-02-04 20:14:17 -0600

    The Movie DB, First commit.

---
+++ MovieGEflix/app/src/main/java/com/diego/moviegeflix/repository/MovieRepositoryImpl.kt
@@ -0,0 +1,32 @@
+package com.diego.moviegeflix.repository
+
+import com.diego.moviegeflix.data.local.LocalMovieDataSource
+import com.diego.moviegeflix.data.model.MovieList
+import com.diego.moviegeflix.data.model.toMovieEntity
+import com.diego.moviegeflix.data.remote.RemoteMovieDataSource
+
+class MovieRepositoryImpl(
+    private val dataSourceRemote: RemoteMovieDataSource,
+    private val dataSourceLocal: LocalMovieDataSource
+) : MovieRepository {
+    override suspend fun getUpcomingMovies(): MovieList{
+        dataSourceRemote.getUpcomingMovies().results.forEach { movie ->
+            dataSourceLocal.saveMovie(movie.toMovieEntity("upcoming"))
+        }
+        return dataSourceLocal.getUpcomingMovies()
+    }
+
+    override suspend fun getTopRatedMovies(): MovieList {
+        dataSourceRemote.getTopRatedMovies().results.forEach { movie ->
+            dataSourceLocal.saveMovie(movie.toMovieEntity("toprated"))
+        }
+        return dataSourceLocal.getTopRatedMovies()
+    }
+    override suspend fun getPopularMovies(): MovieList {
+        dataSourceRemote.getPopularMovies().results.forEach { movie ->
+            dataSourceLocal.saveMovie(movie.toMovieEntity("popular"))
+        }
+        return dataSourceLocal.getPopularMovies()
+    }
+
+}
